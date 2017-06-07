3c99277a4fde4546b172382e6b8d40eea1c18ecd
Miha Pleško <miha.plesko@xlab.si>
2017-06-07 12:15:24 +0200

    Add recipe for eu.mikelangelo-project.osv.cloud-init
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

---
+++ docker_files/recipes/eu.mikelangelo-project.osv.cloud-init/build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+# Clear usr.manifest.skel that is already contained in bootstrap package
+echo "[manifest]" > ${OSV_DIR}/usr.manifest.skel
+
+###############################################################################
+# Build the cloud-init package.
+###############################################################################
+
+echo "Exporting cloud-init"
+
+# Must have HTTP server compiled or else loader fails
+echo "Build prerequsite: httpserver"
+cd ${OSV_DIR}
+${OSV_DIR}/scripts/build image=httpserver
+echo "Build prerequsite done"
+
+# Patch the module file to exclude the dependency on HTTP server
+cd ${OSV_DIR}
+patch -p1 < ${RECIPE_DIR}/cloud-init.patch
+${OSV_DIR}/scripts/build image=cloud-init
+
+cd ${OSV_BUILD_DIR}
+${OSV_DIR}/scripts/upload_manifest.py -m usr.manifest -e ${PACKAGE_RESULT_DIR} -D gccbase=${GCCBASE} -D miscbase=${MISCBASE}
+
+cd ${PACKAGE_RESULT_DIR}
+capstan package init --name "${PACKAGE_NAME}" \
+    --title "cloud-init" \
+    --author "MIKELANGELO Project (info@mikelangelo-project.eu)" \
+    --version 0.1 \
+    --require eu.mikelangelo-project.osv.httpserver
