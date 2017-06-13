c54f91bd96a5cf0d5b0b76904346abe24f53adc1
Miha Pleško <miha.plesko@xlab.si>
2017-06-13 16:02:45 +0200

    Add recipe for eu.mikelangelo-project.openfoam.poroussimplefoam
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

---
+++ docker_files/recipes/eu.mikelangelo-project.openfoam.poroussimplefoam/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+# Clear usr.manifest.skel that is already contained in bootstrap package
+echo "[manifest]" > ${OSV_DIR}/usr.manifest.skel
+
+# Patch the module file to exclude the dependency on HTTP server
+cd ${OSV_DIR}
+patch -p1 < ${COMMON_DIR}/openfoam_remove_ompi.patch
+
+${OSV_DIR}/scripts/build image=OpenFOAM
+
+cd ${OSV_BUILD_DIR}
+echo "/usr/bin/porousSimpleFoam.so: ${OSV_DIR}/mike-apps/OpenFOAM/ROOTFS/usr/bin/porousSimpleFoam.so" > usr.manifest
+${OSV_DIR}/scripts/upload_manifest.py -m usr.manifest -e ${PACKAGE_RESULT_DIR} -D gccbase=${GCCBASE} -D miscbase=${MISCBASE}
+
+cd ${PACKAGE_RESULT_DIR}
+capstan package init --name "${PACKAGE_NAME}" \
+    --title "OpenFOAM porousSimpleFoam" \
+    --author "MIKELANGELO Project (info@mikelangelo-project.eu)" \
+    --version 2.4.0 \
+    --require eu.mikelangelo-project.openfoam.core
