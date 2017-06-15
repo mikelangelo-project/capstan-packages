4d845392d719a6a1bdc88f1198474419b8381c36
Miha Pleško <miha.plesko@xlab.si>
2017-06-15 11:10:01 +0200

    Add recipe for node-6.10.2
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

---
+++ docker_files/recipes/node-6.10.2/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+# Clear usr.manifest.skel that is already contained in bootstrap package
+echo "[manifest]" > ${OSV_DIR}/usr.manifest.skel
+
+NODE_VERSION=6.10.2
+echo "Exporting Node ${NODE_VERSION}"
+
+cd ${OSV_DIR}/apps/node
+./GET ${NODE_VERSION}
+make NODE_VERSION=${NODE_VERSION}
+
+cd ${PACKAGE_RESULT_DIR}
+mkdir ./bin
+cp ${OSV_DIR}/apps/node/libnode-${NODE_VERSION}.so ./bin/
+ln -s /bin/libnode-${NODE_VERSION}.so node
+
+cd ${PACKAGE_RESULT_DIR}
+capstan package init --name "${PACKAGE_NAME}" \
+    --title "NodeJS ${NODE_VERSION}" \
+    --author "MIKELANGELO Project (info@mikelangelo-project.eu)" \
+    --version ${NODE_VERSION}
+
+echo "Done"
