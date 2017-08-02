a11178e7d1d97e4847c6be59d074cf59c4cf1ea5
Miha Pleško <miha.plesko@xlab.si>
2017-08-02 09:02:45 +0200

    Update apache.spark-2.1.1 recipe to version 0.2
    Old recipe for Spark didn't play well anymore due to some changes
    on dependent repositories. Most notably, the osvProcessBuilder repository
    is now configured as a Maven project so manually calling `make` program
    is no longer needed. Also, since it's a Maven project, it puts the
    result in target/ directory and not in some 5-level deeply nested direcotry
    like in case we call `make` manually.
    
    With this commit we mostly simplify the recipe so that it's now nicely
    readable. Also, since we've modified the recipe, we increase packaging
    version from 0.1 to 0.2.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- docker_files/recipes/apache.spark-2.1.1/build.sh
+++ docker_files/recipes/apache.spark-2.1.1/build.sh
@@ -10,18 +10,11 @@ echo "Exporting Apache Spark"
 echo "Build OSv Process Builder (mvn fails to build spark if not)"
 
 cd ${RECIPE_DIR}
-git clone -b feature/pyspark_testing https://github.com/gasper-vrhovsek/osv-process-builder-lib.git
+git clone -b feature/java_so https://github.com/gasper-vrhovsek/osv-process-builder-lib.git
 
-osvpbDir=${RECIPE_DIR}/osv-process-builder-lib/src/main/java/org/mikelangelo/osvprocessbuilder
-cd ${osvpbDir}
-make -j ${CPU_COUNT}
-mvn install:install-file \
- -Dfile=osv-process-builder.jar \
- -DgroupId=org.mikelangelo.osv \
- -DartifactId=osvProcessBuilder \
- -Dversion=0.1 \
- -Dpackaging=jar \
- -T ${CPU_COUNT}
+cd osv-process-builder-lib
+mvn install -T ${CPU_COUNT}
+osvPbDir=${RECIPE_DIR}/osv-process-builder-lib/target
 
 echo "Build Spark"
 
@@ -36,7 +29,7 @@ mkdir ${PACKAGE_RESULT_DIR}/spark
 mkdir -p ${PACKAGE_RESULT_DIR}/spark/launcher/target/scala-2.11
 mkdir -p ${PACKAGE_RESULT_DIR}/usr/lib
 tar xvf ${RECIPE_DIR}/spark/spark-2.1.1-bin-2.7.3.tgz -C ${PACKAGE_RESULT_DIR}/spark --strip-components=1
-cp ${osvpbDir}/stormy-java/libOsvProcessBuilder.so ${PACKAGE_RESULT_DIR}/usr/lib/libOsvProcessBuilder.so
+cp ${osvPbDir}/libOsvProcessBuilder.so ${PACKAGE_RESULT_DIR}/usr/lib/libOsvProcessBuilder.so
 
 echo "Create configuration files for Capstan"
 
@@ -44,6 +37,6 @@ cd ${PACKAGE_RESULT_DIR}
 capstan package init --name "${PACKAGE_NAME}" \
     --title "Apache Spark" \
     --author "MIKELANGELO Project (info@mikelangelo-project.eu)" \
-    --version 0.1 \
+    --version 0.2 \
     --require openjdk8-zulu-compact3-with-java-beans \
     --platform ${PLATFORM}
