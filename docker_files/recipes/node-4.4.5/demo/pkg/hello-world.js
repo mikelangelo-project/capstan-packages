51fcef0f5eb648b108bda7f904bfdd2241e0db71
Miha Pleško <miha.plesko@xlab.si>
2017-10-03 12:20:25 +0200

    License our project as 3-clause BSD
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- docker_files/recipes/node-4.4.5/demo/pkg/hello-world.js
+++ docker_files/recipes/node-4.4.5/demo/pkg/hello-world.js
@@ -1,3 +1,9 @@
+//
+// Copyright (C) 2017 XLAB, Ltd.
+//
+// This work is open source software, licensed under the terms of the
+// BSD license as described in the LICENSE file in the top-level directory.
+//
 
 function printHello(){
 	for(var i = 0; i<10; i++){
