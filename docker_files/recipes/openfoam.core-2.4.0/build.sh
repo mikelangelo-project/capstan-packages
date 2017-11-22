23f8477fdeb1439ed6ee0435f1477cbe64439216
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2017-11-22 13:40:12 +0100

    Only incude single solver so dependency in seperate solver packages

--- docker_files/recipes/openfoam.core-2.4.0/build.sh
+++ docker_files/recipes/openfoam.core-2.4.0/build.sh
@@ -13,19 +13,22 @@ set -o errtrace
 
 # Patch the module file to exclude the dependency on HTTP server
 cd ${OSV_DIR}
-patch -p1 < ${RECIPE_DIR}/openfoam-core.patch
+patch -p1 < ${COMMON_DIR}/openfoam_core.patch
 patch -p1 < ${COMMON_DIR}/openfoam_remove_ompi.patch
 
 ${OSV_DIR}/scripts/build image=OpenFOAM export=all usrskel=none export_dir=$PACKAGE_RESULT_DIR -j ${CPU_COUNT}
 
 # Add decompose and reconstruct that are in the same directory as (ignored) solvers
+mkdir -p ${PACKAGE_RESULT_DIR}/usr/bin
 cp ${OSV_DIR}/mike-apps/OpenFOAM/ROOTFS/usr/bin/decomposePar.so ${PACKAGE_RESULT_DIR}/usr/bin
 cp ${OSV_DIR}/mike-apps/OpenFOAM/ROOTFS/usr/bin/reconstructPar.so ${PACKAGE_RESULT_DIR}/usr/bin
+ln -s ${PACKAGE_RESULT_DIR}/usr/bin/decomposePar.so decomposePar
+ln -s ${PACKAGE_RESULT_DIR}/usr/bin/reconstructPar.so reconstructPar
 
 cd ${PACKAGE_RESULT_DIR}
 capstan package init --name "${PACKAGE_NAME}" \
     --title "OpenFOAM Core" \
     --author "MIKELANGELO Project (info@mikelangelo-project.eu)" \
-    --version 0.3 \
+    --version 0.4 \
     --require ompi-1.10 \
     --platform ${PLATFORM}
--- docker_files/recipes/openfoam.pisofoam-2.4.0/build.sh
+++ docker_files/recipes/openfoam.pisofoam-2.4.0/build.sh
@@ -16,13 +16,18 @@ echo "/usr/bin/pisoFoam.so: ${OSV_DIR}/mike-apps/OpenFOAM/ROOTFS/usr/bin/pisoFoa
 
 # Patch the module file to exclude the dependency on HTTP server
 cd ${OSV_DIR}
+patch -p1 < ${COMMON_DIR}/openfoam_core.patch
 patch -p1 < ${COMMON_DIR}/openfoam_remove_ompi.patch
-${OSV_DIR}/scripts/build image=OpenFOAM export=all usrskel=none export_dir=$PACKAGE_RESULT_DIR -j ${CPU_COUNT}
+${OSV_DIR}/scripts/build image=OpenFOAM export=none usrskel=none -j ${CPU_COUNT}
+
+# Add pisoFoam solver
+mkdir -p ${PACKAGE_RESULT_DIR}/usr/bin
+cp ${OSV_DIR}/mike-apps/OpenFOAM/ROOTFS/usr/bin/pisoFoam.so ${PACKAGE_RESULT_DIR}/usr/bin
 
 cd ${PACKAGE_RESULT_DIR}
 capstan package init --name "${PACKAGE_NAME}" \
     --title "OpenFOAM pisoFoam" \
     --author "MIKELANGELO Project (info@mikelangelo-project.eu)" \
-    --version 0.2 \
+    --version 0.3 \
     --require openfoam.core-2.4.0 \
     --platform ${PLATFORM}
