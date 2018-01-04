51de219b0011eb7d21e67e941a73ce23096e0c83
Miha Pleško <miha.plesko@xlab.si>
2018-01-04 10:05:05 +0100

    Perform Docker cleanup in CRON script
    Docker was consuming more and more HDD space with each run
    of the run.sh script due to staled images and containers.
    With this commit we therefore perform a complete cleanup before
    each run to avoid out-of-space problem.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- run.sh
+++ run.sh
@@ -6,6 +6,12 @@
 # BSD license as described in the LICENSE file in the top-level directory.
 #
 
+#
+# This script is meant to be run by CRON to periodically rebuild all packages
+# from scratch on the latest OSv master. The build will take two hours or so,
+# therefore don't run it too often.
+#
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -14,6 +20,10 @@ set -o errtrace
 echo "Pull latest code"
 git fetch upstream
 git checkout upstream/master
+
+echo "Remove old images and containers"
+docker rm mikelangelo/capstan-packages --force || true # forcibly stop old container if it's still running for some reason
+docker system prune -a --force # remove all unused images and containers
 
 echo "Build container from scratch"
 docker build -t mikelangelo/capstan-packages . --no-cache
