09e6f78e15ae71814a225d7fce7e9079a8a4dec8
Miha Pleško <miha.plesko@xlab.si>
2018-01-04 15:53:43 +0100

    Adjust classpath to new spark package structure
    Recently the apache.spark-2.1.1 package was restructured to
    have project directory in / (instead /spark). With this commit
    we reflect this also in its run configurations.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- docker_files/recipes/apache.spark-2.1.1/build.sh
+++ docker_files/recipes/apache.spark-2.1.1/build.sh
@@ -42,6 +42,6 @@ cd ${PACKAGE_RESULT_DIR}
 capstan package init --name "${PACKAGE_NAME}" \
     --title "Apache Spark" \
     --author "MIKELANGELO Project (info@mikelangelo-project.eu)" \
-    --version 0.4 \
+    --version 0.5 \
     --require openjdk8-zulu-compact3-with-java-beans \
     --platform ${PLATFORM}
